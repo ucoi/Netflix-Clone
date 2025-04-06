b04d2c3b94aee1be20a43e87782ee3f5a7158fb6
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-04-06 10:58:13 +0200

    Forming the bacckend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api/v1/auth", authRoutes); // if u visit this route it will go to authRoutes
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
