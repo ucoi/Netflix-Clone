eb111762a1642b20fe4de1f6ab9ff05c3391ba3a
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-04-06 13:29:36 +0200

    too many things done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,26 @@
+import User from "../models/user.model.js";
+
 export async function signup(req, res) {
-  res.send("Signup route ");
+  try {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(email) === false) {
+      return res.status(400).json({ message: "Please enter a valid email" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const existinguserbyemail = await User.findOne({ email: email });
+
+    if (existinguserbyemail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+  } catch (error) {}
 }
 
 export async function login(req, res) {
@@ -9,4 +30,3 @@ export async function login(req, res) {
 export async function logout(req, res) {
   res.send("Logout route ");
 }
-
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+app.use(express.json()); 
+const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1/auth", authRoutes); // if u visit this route it will go to authRoutes
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+  connectDB();
 });
